5f5a9805cd91782999203e8cff348acfcb2db9bf
Kevin Lundin <kevinlundin@comcast.net>
2024-09-17 19:55:49 -0400

    finished isValid

--- src/objects.ts
+++ src/objects.ts
@@ -42,7 +42,16 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type == "short_answer_question") {
+        return true;
+    } else {
+        for (let i = 0; i < question.options.length; i++) {
+            if (question.options[i] == answer) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 /**
