125932002fc9e1fdc06ae05260b0d3c2e0d95681
Kevin Lundin <kevinlundin@comcast.net>
2024-09-19 19:17:47 -0400

    Finished getNames

--- src/nested.ts
+++ src/nested.ts
@@ -84,7 +84,11 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    let names: string[] = [];
+    for (let i = 0; i < questions.length; i++) {
+        names.push(questions[i].name);
+    }
+    return [...names];
 }
 
 /***
