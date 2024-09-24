6d7cd7ed3a1749fa3ed4ab60b651ea235e4cd6a8
Kevin Lundin <kevinlundin@comcast.net>
2024-09-23 23:20:14 -0400

    Finished CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,58 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+//Holidays: Christmas, Halloween, New Years, St Patrick's, Thanksgiving
+export function alphabeticalCycle(holiday: string): string {
+    switch (holiday) {
+        case "🎅":
+            return "🎃";
+        case "🎃":
+            return "🎆";
+        case "🎆":
+            return "☘️";
+        case "☘️":
+            return "🦃";
+        case "🦃":
+            return "🎅";
+    }
+    return "";
+}
+
+export function dateCycle(holiday: string): string {
+    switch (holiday) {
+        case "🎆":
+            return "☘️";
+        case "☘️":
+            return "🎃";
+        case "🎃":
+            return "🦃";
+        case "🦃":
+            return "🎅";
+        case "🎅":
+            return "🎆";
+    }
+    return "";
+}
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🎅");
+    return (
+        <div>
+            Holiday: {holiday}
+            <Button
+                onClick={() => {
+                    setHoliday(alphabeticalCycle(holiday));
+                }}
+            >
+                Cycle by Alphabet
+            </Button>
+            <Button
+                onClick={() => {
+                    setHoliday(dateCycle(holiday));
+                }}
+            >
+                Cycle by Year
+            </Button>
+        </div>
+    );
 }
